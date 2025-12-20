ef04abafa67e8ce2c877eaa06fa2f2ebeb6c9485
Marek Brzezinski <marek.j.brzezinski@gmail.com>
2025-12-20 23:30:46 +0100

    Response metadata added

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ def main():
         model="gemini-2.5-flash",
         contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
     )
+    if response.usage_metadata is None:
+        raise RuntimeError("Could't get Api response")
+    print(
+        f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}"
+    )
     print(response.text)
 
 
